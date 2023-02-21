99fe41d939d6e1218a8a25591adf4013832ad8ac
Anderson Costa <117184997+Andersoncostaf@users.noreply.github.com>
2023-02-21 12:26:44 +0000

    Problema finalizado em 21/02:
     Multiplos de 3 substituido por FIZZ
    Multiplos de 5 substituido por BUZZ
    Multiplos de 3 e 5 substituido por FIZZBUZZ
    
    [1,2,3,4,5,15] -> [1,2,Fizz, 4, Buzz, FizzBuzz]
    [1,2,4,8,10]  -> [1,2,4,8, Buzz]
    
    detalhe importante: Ler a lista de números de um arquivo!
    Caso a lista não exista, você deve exibir uma mensagem de erro para o usuário.

--- lib/fizz_buzz/fizz_buzz.ex
+++ lib/fizz_buzz/fizz_buzz.ex
@@ -1,12 +1,30 @@
 defmodule FizzBuzz do 
-
-  def build(file_name) do
-    |> File.read()
-    |> handle_file_read()
-   # file = File.read(file_name)
-    handle_file_read()
+  def build(file_name) do 
+    file_name #tenho um aquivo
+    |> File.read() #leio o arquivo
+    |> handle_file_read() #lido com essa leitura
   end
 
-  def handle_file_read({:ok, result}), do: result
-  def handle_file_read({:error, reason})
+  defp handle_file_read({:ok, result}) do # se essa leitura der sucesso
+    result = 
+      result
+      |> String.split(",") # vou "explitar a minha string"
+      |> Enum.map(&convert_and_evaluate_numbers/1) #converte cada elemento da minha string
+
+    {:ok, result}
+  end
+
+  defp handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  defp convert_and_evaluate_numbers(elem) do # então  cada elemento eu vou.... 
+    elem
+    |> String.to_integer() #converte para inteiro
+    |> evaluate_numbers() #verificar esse inteiro.
+  end
+
+  defp evaluate_numbers(number) when rem(number,3) == 0 and rem (number,5) == 0, do:fizzbuzz
+  defp evaluate_numbers(number) when rem(number,3) == 0, do:fizz
+  defp evaluate_numbers(number) when rem(number,3) == 0, do:buzz
+  defp evaluate_numbers(number), do: number
+
 end
